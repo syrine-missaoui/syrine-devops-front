d04896b09407ba7bf9a77156301b5583fa4ce1ba
syrine-missaoui <missaoui.syrine@esprit.tn>
2023-11-02 15:56:32 +0000

    change url ip

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl = 'http://localhost:8082'; 
+  private apiUrl = 'http://192.168.10.10:8082'; 
 
   constructor(private http: HttpClient) { }
 
